ededb79a72a354102ea29a95a7278b498da856c9
Juan Volpi <103449292+JuanVolpi@users.noreply.github.com>
2023-05-09 16:02:37 +0100

    Add files via upload

---
+++ JogoDaMemória/src/menu-inicial.js
@@ -0,0 +1,17 @@
+function moveCurtains() {
+    var curtain1 = document.querySelector('.curtain1');
+    var curtain2 = document.querySelector('.curtain2');
+
+    curtain1.style.left = '150px';
+    curtain2.style.right = '150px';
+    console.log('oi');
+}
+
+function repoCurtains() {
+    var curtain1 = document.querySelector('.curtain1');
+    var curtain2 = document.querySelector('.curtain2');
+
+    curtain1.style.left = '200px';
+    curtain2.style.right = '200px';
+    console.log('oi');
+}
