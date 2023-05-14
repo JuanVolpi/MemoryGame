f3ad516887526ee18a14dca76d70dd0e4221d694
Tomas Marques <ninbo.sum@pm.me>
2023-05-14 17:10:10 +0100

    mini.... sry

--- src/menu-dificuldade.js
+++ src/menu-dificuldade.js
@@ -60,7 +60,7 @@ Array.from(cartas).forEach(
  * de tempo prefedefinido na funcção
  */
 function gameStartRedirect(gameMusicTheme, dificuldade) {
-  playSound(gameMusicTheme, 0.5);
+  playSound(gameMusicTheme, 0.3);
 
   document.body.style.animationName = "fadeOut";
   localStorage.setItem("dificuldade", dificuldade);
